1de6fea159299a447fa81fd4152dfa1489917ce0
Robin Bastian <robin.bastian@outlook.com>
2025-07-18 14:27:09 +0700

    add v2_sales_drive_final and its workflow dispatch

---
+++ models/v2/v2_sales_drive_final.sql
@@ -0,0 +1,18 @@
+{{ 
+  config(
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    on_schema_change = 'append_new_columns',
+    unique_key = ['id'],
+    tags = ['v2', 'sales', 'drive', 'final']
+  )
+}}
+
+select
+  id, po_date, year_month_po, year_po, month_po, product, variant, store_name_ori, group_account, type, sub_type, channel, dist,
+  area_simple, city, province, dist_branc, dist_kode_outlet, dist_outlet_name, sales_pcs, sales_ctn, sales_qty3, price_per_pc_rev,
+  sales_value_gtv, sales_value_rev, sales_value_rbp, account_manager, supervisor, price_per_pc_rbp, store_name_clean, delivery_date,
+  po_pcs, po_ctn, po_value_rbp, smd, sales_supervisor, sub_area
+from alamii.sales_production.v2_sales_drive_staging
+where date(ingest_at) = date("2025-07-17")
+qualify row_number() over(partition by id order by ingest_at desc) = 1
