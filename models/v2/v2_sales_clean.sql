adf533bad5ebfbe50f8bd657e1fd0c133267473d
Robin Bastian <robin.bastian@outlook.com>
2025-08-28 11:21:07 +0700

    add store_status and sales_type to v2 models

--- models/v2/v2_sales_clean.sql
+++ models/v2/v2_sales_clean.sql
@@ -9,12 +9,12 @@ select
   id, po_date, year_month_po, year_po, month_po, product, variant, store_name_ori, group_account, type, sub_type, channel, dist,
   area_simple, city, province, dist_branc, dist_kode_outlet, dist_outlet_name, sales_pcs, sales_ctn, sales_qty3, price_per_pc_rev,
   sales_value_gtv, sales_value_rev, sales_value_rbp, account_manager, supervisor, price_per_pc_rbp, store_name_clean, delivery_date,
-  po_pcs, po_ctn, po_value_rbp, smd, sales_supervisor, sub_area
+  po_pcs, po_ctn, po_value_rbp, smd, sales_supervisor, sub_area, store_status, sales_type
 from {{ ref("sales_clean_model") }}
 union all
 select
   id, po_date, year_month_po, year_po, month_po, product, variant, store_name_ori, group_account, type, sub_type, channel, dist,
   area_simple, city, province, dist_branc, dist_kode_outlet, dist_outlet_name, sales_pcs, sales_ctn, sales_qty3, price_per_pc_rev,
   sales_value_gtv, sales_value_rev, sales_value_rbp, account_manager, supervisor, price_per_pc_rbp, store_name_clean, delivery_date,
-  po_pcs, po_ctn, po_value_rbp, smd, sales_supervisor, sub_area
+  po_pcs, po_ctn, po_value_rbp, smd, sales_supervisor, sub_area, store_status, sales_type
 from {{ ref("v2_sales_drive_final") }}
